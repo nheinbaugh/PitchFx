d5ba602d1f298762f16d76890ad379a6d9ec6050
Nick Heinbaugh <nick.heinbaugh@live.com>
2015-03-15 21:20:09 -0700

    Added simple pitcher controller

---
+++ PitchFx.Api/Controllers/PitcherController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace PitchFx.Api.Controllers
+{
+    public class PitcherController : ApiController
+    {
+        // GET api/Pitcher/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
