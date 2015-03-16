658e739a46fbdf8a5dff4580b1e26f208eb06b6d
Nick Heinbaugh <nick.heinbaugh@live.com>
2015-03-15 21:14:23 -0700

    Updated Value Controller
    Simple test to force a build

--- PitchFx.Api/Controllers/ValuesController.cs
+++ PitchFx.Api/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace PitchFx.Api.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "bob";
         }
 
         // POST api/values
